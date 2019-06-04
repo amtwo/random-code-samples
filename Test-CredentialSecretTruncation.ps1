2a76f60b8e7692f94c0bdfe6b8826e55ad6dd7d5
Andy Mallon (ᴀᴍ²) <amtwo@users.noreply.github.com>
2019-06-03 20:59:04 -0400

    Update Test-CredentialSecretTruncation.ps1

--- Test-CredentialSecretTruncation.ps1
+++ Test-CredentialSecretTruncation.ps1
@@ -1,3 +1,27 @@
+<#
+.SYNOPSIS
+    This script will create credentials & test the secret to see that they are equal to the intended value.
+.DESCRIPTION
+    This script will create an arbitrary number of credentials using the name format of AM2_xxx and validate that the secret 
+    saved in the database matches the value passed as a parameter. After testing, all created credentials will be deleted. 
+    
+    This script depends on having Get-MSSQLCredentialPasswords saved in the same directory as this script. 
+    Get-MSSQLCredentialPasswords can be downloaded from GitHub:
+    https://github.com/NetSPI/Powershell-Modules/blob/master/Get-MSSQLCredentialPasswords.psm1
+    
+.PARAMETER secret
+    String to be used as the Secret in a series of CREATE CREDENTIAL statements.
+.PARAMETER identity
+    String to be used as the Indentity in a series of CREATE CREDENTIAL statements.
+.PARAMETER credCount
+    Number of CREATE CREDENTIAL statements to use
+
+.EXAMPLE
+    .\Test-CredentialSecretTruncation.ps1 -secret "SomePassword()123" -credCount 100
+    This will create 100 credentials using the secret "SomePassword()123", and the default identity of "Shared Access Signature".
+.NOTES
+#>
+
 Param (
     [string] $secret,
     [string] $identity = "Shared Access Signature",
